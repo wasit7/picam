adebf8c079083cb6f263ee47a92b0b1488b31a34
wasit7 <wasit7@gmail.com>
2016-06-14 10:19:06 +0700

    updateGetImages

--- temp/getimages.sh
+++ temp/getimages.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #raspistill -o test.jpg -w 3456 -h 2592 -t 999999
 #raspistill -o test.png -w 1920 -h 1080 -t 99999
+mkdir -p ~/Desktop/img
 DATE=$(date +"%y%m%d_%H%M%S")
 # raspistill -o ../img/[$DATE]_%02d.jpg -st -t 0 -k -mm average -q 90 -v -awb none -co 0 -sh 90 -sa 50 -p 50,50,972,729 -ISO 200 -ev 0 -ifx none
 #raspistill -o ../img/[$DATE]_%02d.jpg -t 0 -k -ex off -mm average -q 90 -v -awb off -co 0 -sh 80 -p 100,100,972,729 -ISO 800
-raspistill -o ../img/[$DATE]_%02d.jpg -st -t 0 -k -mm average -q 90 -v -awb none -p 50,50,972,729 -ifx none
+raspistill -o ~/Desktop/img/[$DATE]_%02d.jpg -st -t 0 -k -mm average -q 90 -v -awb none -p 50,50,972,729 -ifx none
